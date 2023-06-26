0cc3d49e841019cf6490e3a2479c9156f62cfc5b
Theo Gravity <theo@switchboard.app>
2023-06-26 17:06:47 -0500

    Update SDKs (#11)

--- sdk/go/fusionauth/getTenant.go
+++ sdk/go/fusionauth/getTenant.go
@@ -28,7 +28,7 @@ import (
 // import (
 //
 //	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-//	"github.com/theogravity/pulumi-fusionauth/sdk/go/fusionauth"
+//	"github.com/theogravity/pulumi-fusionauth/sdk/v3/go/fusionauth"
 //
 // )
 //
