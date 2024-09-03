14ca999522fcfc128dbdce9a5d932c7698330958
Bri Hatch <bri@ifokr.org>
2024-09-02 21:43:40 -0700

    Fix the python packagename
    i.e. s/-/_/ in theogravity_pulumi-fusionauth

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -17,7 +17,7 @@ def readme():
         return "fusionauth Pulumi Package - Development Version"
 
 
-setup(name='theogravity_pulumi-fusionauth',
+setup(name='theogravity_pulumi_fusionauth',
       python_requires='>=3.8',
       version=VERSION,
       description="A Pulumi package for managing FusionAuth instances.",
